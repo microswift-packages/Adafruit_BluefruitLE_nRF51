ee6aebb3ecbc03800a6e09c778d8a9eddf7d0146
Carl Peto <carl.peto@me.com>
2023-08-28 21:41:07 +0100

    match library name to package/repository name

--- Package.swift
+++ Package.swift
@@ -3,18 +3,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "Adafruit_BLE",
+    name: "Adafruit_BluefruitLE_nRF51",
     products: [
         .library(
-            name: "Adafruit_BLE",
-            targets: ["Adafruit_BLE"]),
+            name: "Adafruit_BluefruitLE_nRF51",
+            targets: ["Adafruit_BluefruitLE_nRF51"]),
     ],
     dependencies: [
         .package(url: "https://github.com/microswift-packages/Arduino", from: "1.0.0"),
     ],
     targets: [
         .target(
-            name: "Adafruit_BLE",
+            name: "Adafruit_BluefruitLE_nRF51",
             dependencies: ["Arduino"],
             path: "microswift",
             sources: ["main.swift"]),
